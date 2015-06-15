aa5ab0cf42228720c1398a2a4f02a30941544e4e
Lee HyangSeok <eekek@naver.com>
2015-06-15 11:51:05 +0900

    mapper package repository package vo package
    pom.xml
    web.xml
    applicationContext.xml

---
+++ src/mapper/UserMapper.java
@@ -0,0 +1,11 @@
+package mapper;
+
+import java.util.List;
+
+import vo.User;
+
+public interface UserMapper {
+	public int insert(User user);
+	public int update(User user);
+	public List<User> selectList();
+}
