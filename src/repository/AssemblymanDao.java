c707bc0458774c51e41db285436749ee24c62bbb
seongsan14 <eekek@naver.com>
2015-07-08 23:25:25 +0900

    ...

--- src/repository/AssemblymanDao.java
+++ src/repository/AssemblymanDao.java
@@ -7,9 +7,11 @@ import mapper.AssemblymanMapper;
 
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import vo.Assemblyman;
 
+@Component
 public class AssemblymanDao {
 	private SqlSessionTemplate session;
 
